f498899dfdf5d22223db7c6c528c939bc801257d
MiheerSharma22 <miheer.sharma1@gmail.com>
2023-08-10 16:50:08 +0530

    style: added shadows to selections

--- src/components/Home.js
+++ src/components/Home.js
@@ -17,7 +17,7 @@ const Home = ({ setSelected }) => {
   return (
     <div className="relative w-[250px] h-[220px] mt-[9rem] icons-container">
       <button
-        className="rock-button bg-white rounded-full w-[110px] aspect-square absolute top-[-2rem] left-[-2rem] grid place-items-center border-[12px] border-red-500"
+        className="rock-button bg-white shadow-[0px_7px_0px_0px_rgba(181,51,51,1)] rounded-full w-[110px] aspect-square absolute top-[-2rem] left-[-2rem] grid place-items-center border-[12px] border-red-500"
         onClick={clickHandler}
         name="rock"
       >
@@ -25,7 +25,7 @@ const Home = ({ setSelected }) => {
       </button>
 
       <button
-        className="rock-button bg-white rounded-full w-[110px] aspect-square absolute top-[-2rem] right-[-2rem] grid place-items-center border-[12px] border-blue-500 "
+        className="rock-button bg-white shadow-[0px_7px_0px_0px_rgba(16,81,133,1)] rounded-full w-[110px] aspect-square absolute top-[-2rem] right-[-2rem] grid place-items-center border-[12px] border-blue-500 "
         onClick={clickHandler}
         name="paper"
       >
@@ -33,7 +33,7 @@ const Home = ({ setSelected }) => {
       </button>
 
       <button
-        className="rock-button bg-white rounded-full w-[110px] aspect-square absolute bottom-[-2rem] left-[30%] grid place-items-center border-[12px] border-yellow-400"
+        className="rock-button bg-white shadow-[0px_7px_0px_0px_rgba(194,140,23,1)] rounded-full w-[110px] aspect-square absolute bottom-[-2rem] left-[30%] grid place-items-center border-[12px] border-yellow-400"
         onClick={clickHandler}
         name="scissor"
       >
