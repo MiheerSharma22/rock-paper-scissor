24f83201dedb1b2173b0eeff58d88ded2998f3a5
MiheerSharma22 <miheer.sharma1@gmail.com>
2023-08-11 13:16:16 +0530

    style: changed size of the score card container and also changed its border styles

--- src/components/ScoreCard.js
+++ src/components/ScoreCard.js
@@ -3,13 +3,13 @@ import "./ScoreCard.css";
 
 const ScoreCard = ({ score }) => {
   return (
-    <div className="score-card-container flex md:min-w-[40%] py-[1rem] px-[1rem] justify-between border-4 border-headerOutline rounded-lg">
-      <div className="heading text-white font-bold flex flex-col text-[1.5rem] md:text-[2rem] uppercase h-fit w-[26%] p-0 gap-0 leading-none">
+    <div className="score-card-container flex md:min-w-[50%] py-[1rem] px-[1rem] justify-between md:mt-0 mt-3 border-[3px] border-headerOutline rounded-xl">
+      <div className="heading text-white font-extrabold  flex flex-col text-[1.5rem] md:text-[2rem] uppercase h-fit w-[16%] p-0 gap-0 leading-none">
         <p>Rock Paper Scissors</p>
       </div>
 
-      <div className="scoreboard bg-white min-w-[25%] p-[0.5rem] flex flex-col rounded-md leading-none">
-        <span className="uppercase tracking-widest grid place-items-center text-scoreText  font-[600] text-[1.25rem] pt-[0.25rem]">
+      <div className="scoreboard bg-white min-w-[20%] p-[0.5rem] flex flex-col rounded-md leading-none">
+        <span className="uppercase tracking-widest grid place-items-center text-scoreText  font-[600] text-sm pt-[0.25rem]">
           Score
         </span>
         <p className=" h-full grid place-items-center text-[2.75rem] text-darkText font-extrabold ">
